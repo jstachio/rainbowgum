d5aeb94a6afa95751ab328d4622138c47cce87b4
Adam Gent <adam.gent@snaphop.com>
2023-09-30 05:11:22 -0400

    Fix test

--- core/src/test/java/io/jstach/rainbowgum/LogConfigTest.java
+++ core/src/test/java/io/jstach/rainbowgum/LogConfigTest.java
@@ -3,6 +3,7 @@ package io.jstach.rainbowgum;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.lang.System.Logger.Level;
+import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,14 +11,13 @@ class LogConfigTest {
 
 	@Test
 	void test() {
-		System.setProperty("rainbowgum.log.stuff", "DEBUG");
+		// System.setProperty("rainbowgum.log.stuff", "DEBUG");
 
-		var resolver = LogConfig.of().levelResolver();
+		var config = LogConfig.of(Map.<String, String>of("rainbowgum.log.stuff", "DEBUG")::get);
+		var resolver = config.levelResolver();
 		var actual = resolver.resolveLevel("stuff");
 
 		assertEquals(Level.DEBUG, actual);
-
-		assertTrue(LogConfig.of().levelResolver().isEnabled("stuff", Level.DEBUG));
 	}
 
 }
