c75ea6e51a353248fcfcafc7e9b263984c07aa4f
Adam Gent <adam.gent@snaphop.com>
2024-01-05 10:36:51 -0500

    Use moditect for apt module

--- rainbowgum-rabbitmq/src/main/java/module-info.java
+++ rainbowgum-rabbitmq/src/main/java/module-info.java
@@ -7,5 +7,4 @@ module io.jstach.rainbowgum.rabbitmq {
 	requires io.jstach.rainbowgum;
 	requires com.rabbitmq.client;
 	requires static org.eclipse.jdt.annotation;
-	requires static io.jstach.rainbowgum.apt;
 }
