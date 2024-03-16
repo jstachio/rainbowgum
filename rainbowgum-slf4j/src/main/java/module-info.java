014a00c03603d5a4cb60b3d3d4b7ce61f116a1cf
Adam Gent <adam.gent@snaphop.com>
2024-03-16 08:37:15 -0400

    Cleanup SLF4J module doc

--- rainbowgum-slf4j/src/main/java/module-info.java
+++ rainbowgum-slf4j/src/main/java/module-info.java
@@ -8,13 +8,19 @@ import io.jstach.rainbowgum.slf4j.RainbowGumSLF4JServiceProvider;
  * <li><a href="https://slf4j.org/manual.html#mdc">MDC</a></li>
  * <li><a href="https://slf4j.org/manual.html#fluent">Fluent Logging API</a></li>
  * <li>{@linkplain io.jstach.rainbowgum.slf4j.spi.LoggerDecoratorService Decorating loggers}</li>
- * <li>{@linkplain io.jstach.rainbowgum.LogEvent.Caller Caller Information}<li>
+ * <li>{@linkplain io.jstach.rainbowgum.LogEvent.Caller Caller Information}</li>
  * </ul>
  * However there is currently no {@link org.slf4j.Marker}
- * support. Furthermore the key value pair support
- * in {@link org.slf4j.spi.LoggingEventBuilder} works by overlaying on top
+ * support. 
+ * <p>
+ * Rainbow Gum supports key value pairs
+ * in {@link org.slf4j.spi.LoggingEventBuilder} by overlaying on top
  * of the current MDC at the time the event is constructed and put into the
  * {@link io.jstach.rainbowgum.LogEvent#keyValues() events key values}.
+ * The value parameter in 
+ * {@link org.slf4j.spi.LoggingEventBuilder#addKeyValue(String,Object)} are
+ * converted to {@link java.lang.String} immediatly as only String values are
+ * supported at this time.
  * <p>
  * Rainbow Gum SLF4J implementation is unique in that it has 
  * two special implementation of loggers.
@@ -24,10 +30,12 @@ import io.jstach.rainbowgum.slf4j.RainbowGumSLF4JServiceProvider;
  * </ul>
  * Other looging implementations like Logback by default use something analogous to changing loggers
  * which require a constant check if the level threshold has changed.
- * Level loggers do not need to that check. Unless changing loggers is turned on by default Level 
- * Loggers are used which are close to zero cost for discarding events.
+ * Level loggers do not need to that check. Unless 
+ * {@linkplain io.jstach.rainbowgum.LogConfig.ChangePublisher changing loggers is turned on} 
+ * by default Level Loggers are used which are close to zero cost for discarding events.
  * 
  * @provides org.slf4j.spi.SLF4JServiceProvider
+ * @see io.jstach.rainbowgum.LogConfig.ChangePublisher
  */
 module io.jstach.rainbowgum.slf4j {
 	
