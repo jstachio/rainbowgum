9c7583d3c78e2c80e31d71267f8da9e745b642d0
Adam Gent <adam.gent@snaphop.com>
2024-04-03 18:55:44 -0400

    Add registry test

--- core/src/main/java/io/jstach/rainbowgum/LogEncoderRegistry.java
+++ core/src/main/java/io/jstach/rainbowgum/LogEncoderRegistry.java
@@ -3,11 +3,8 @@ package io.jstach.rainbowgum;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.EnumMap;
-import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Objects;
-import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.function.Supplier;
 
@@ -19,20 +16,6 @@ import io.jstach.rainbowgum.format.StandardEventFormatter;
  * Encoder registry
  */
 public sealed interface LogEncoderRegistry extends EncoderProvider {
-
-	/**
-	 * Finds an encoder by name.
-	 * @param name encoder name.
-	 * @return encoder
-	 */
-	public Optional<LogEncoder> encoder(String name);
-
-	/**
-	 * Registers an encoder by name.
-	 * @param name encoder name.
-	 * @param encoder loaded encoder
-	 */
-	public void register(String name, LogEncoder encoder);
 
 	/**
 	 * Registers an encoder by uri scheme.
@@ -68,18 +51,6 @@ public sealed interface LogEncoderRegistry extends EncoderProvider {
 final class DefaultEncoderRegistry extends ProviderRegistry<LogEncoder.EncoderProvider, LogEncoder, RuntimeException>
 		implements LogEncoderRegistry {
 
-	private Map<String, LogEncoder> encoders = new ConcurrentHashMap<>();
-
-	@Override
-	public Optional<LogEncoder> encoder(String name) {
-		return Optional.ofNullable(encoders.get(name));
-	}
-
-	@Override
-	public void register(String name, LogEncoder encoder) {
-		encoders.put(name, encoder);
-	}
-
 	private static URI normalize(URI uri) {
 		String scheme = uri.getScheme();
 		String path = uri.getPath();
@@ -112,24 +83,12 @@ final class DefaultEncoderRegistry extends ProviderRegistry<LogEncoder.EncoderPr
 		if (scheme == null) {
 			throw new IllegalStateException("bug. uri was not normalized");
 		}
-		if (scheme.equals("name")) {
-			String _name = uri.getHost();
-			if (_name == null) {
-				_name = name;
-			}
-			return _encoder(uri, name, _name);
 
-		}
 		var provider = providers.get(scheme);
 		if (provider == null) {
 			throw new NoSuchElementException("No encoder found. URI=" + uri);
 		}
 		return provider.provide(uri, name, properties);
-	}
-
-	private LogEncoder _encoder(URI uri, String name, String resolvedName) {
-		return encoder(resolvedName).orElseThrow(() -> new NoSuchElementException(
-				"No encoder found. resolved='" + resolvedName + "', name='" + name + "', uri='" + uri + "'"));
 	}
 
 	private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
---
+++ core/src/test/java/io/jstach/rainbowgum/LogEncoderRegistryTest.java
@@ -0,0 +1,103 @@
+package io.jstach.rainbowgum;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+import java.lang.System.Logger.Level;
+import java.net.URI;
+
+import org.junit.jupiter.api.Test;
+
+import io.jstach.rainbowgum.LogEncoder.EncoderProvider;
+import io.jstach.rainbowgum.LogOutput.OutputProvider;
+import io.jstach.rainbowgum.LogOutput.OutputType;
+import io.jstach.rainbowgum.output.ListLogOutput;
+import io.jstach.rainbowgum.spi.RainbowGumServiceProvider.Configurator;
+
+class LogEncoderRegistryTest {
+
+	ListLogOutput output = new ListLogOutput();
+
+	@Test
+	void testProvide() {
+		var config = LogConfig.builder()
+			.configurator(new EncoderConfigurator())
+			.configurator(new OutputConfigurator())
+			.properties(LogProperties.builder().fromProperties("""
+					logging.appenders=list
+					logging.appender.list.encoder=custom
+					logging.appender.list.output=custom
+					""").build())
+			.build();
+
+		var gum = RainbowGum.builder(config).build();
+		try (var g = gum.start()) {
+			g.router().eventBuilder("stuff", Level.INFO).message("hello").log();
+		}
+		String actual = output.toString();
+		String expected = """
+				CUSTOM hello
+				""";
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void testSetEncoderForOutputType() {
+		var config = LogConfig.builder()
+			.configurator(new EncoderConfigurator())
+			.configurator(new OutputConfigurator())
+			.properties(LogProperties.builder().fromProperties("""
+					logging.appenders=list
+					logging.appender.list.output=custom
+					""").build())
+			.build();
+
+		var gum = RainbowGum.builder(config).build();
+		try (var g = gum.start()) {
+			g.router().eventBuilder("stuff", Level.INFO).message("hello").log();
+		}
+		String actual = output.toString();
+		String expected = """
+				OUTPUT_TYPE hello
+				""";
+		assertEquals(expected, actual);
+	}
+
+	class EncoderConfigurator implements Configurator {
+
+		@Override
+		public boolean configure(LogConfig config) {
+			EncoderProvider provider = new EncoderProvider() {
+
+				@Override
+				public LogEncoder provide(URI uri, String name, LogProperties properties) {
+					return LogFormatter.builder().text("CUSTOM ").message().newline().encoder();
+				}
+			};
+			config.encoderRegistry().register("custom", provider);
+			config.encoderRegistry()
+				.setEncoderForOutputType(OutputType.MEMORY,
+						() -> LogFormatter.builder().text("OUTPUT_TYPE ").message().newline().encoder());
+			return true;
+		}
+
+	}
+
+	class OutputConfigurator implements Configurator {
+
+		@Override
+		public boolean configure(LogConfig config) {
+			OutputProvider provider = new OutputProvider() {
+
+				@Override
+				public LogOutput provide(URI uri, String name, LogProperties properties) throws IOException {
+					return output;
+				}
+			};
+			config.outputRegistry().register("custom", provider);
+			return true;
+		}
+
+	}
+
+}
