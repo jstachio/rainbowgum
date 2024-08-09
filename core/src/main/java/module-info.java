7681456c6c2b1e6deba09cc185dcf2e5c1036157
Adam Gent <adam.gent@snaphop.com>
2024-08-09 13:35:02 -0400

    Add a note on thread safety.

--- core/src/main/java/module-info.java
+++ core/src/main/java/module-info.java
@@ -1,7 +1,11 @@
 /**
  * Core module for RainbowGum which provides low level components for logging as well as a builder 
  * for creating custom RainbowGums. Use {@link io.jstach.rainbowgum.RainbowGum#builder()} to
- * get starting configuring.
+ * get started configuring.
+ * <p>
+ * <strong>Thread Safety:</strong>
+ * With the exception of builders, buffers, and outputs this modules components are
+ * all threadsafe. Output thread safety is protected by Appenders which are threadsafe.
  * 
  * @see io.jstach.rainbowgum
  * @uses io.jstach.rainbowgum.spi.RainbowGumServiceProvider
