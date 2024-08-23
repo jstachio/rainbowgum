cad99630c4d76511ec14a8c5b35260b4c137e8bf
Adam Gent <adam.gent@snaphop.com>
2024-08-23 16:55:44 -0400

    Temp boot commit

---
+++ spring/rainbowgum-spring-boot-starter/src/main/java/io/jstach/rainbowgum/spring/boot/starter/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Please refer to the overview for installation. This bundle is just to make the javadoc
+ * system in Maven work.
+ */
+@org.eclipse.jdt.annotation.NonNullByDefault
+package io.jstach.rainbowgum.spring.boot.starter;
---
+++ spring/rainbowgum-spring-boot-starter/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+/**
+ * Spring Boot Rainbow Gum Integration.
+ * Please refer to the overview for installation.
+ */
+module io.jstach.rainbowgum.spring.boot.starter {
+	requires static org.eclipse.jdt.annotation;
+}
