a3b937b17bb69aa2a9f5e4628066a0b1e7231d83
Adam Gent <adam.gent@snaphop.com>
2024-10-28 12:56:55 -0400

    Fix strange javadoc issue with strong tag

--- core/src/main/java/io/jstach/rainbowgum/LogOutput.java
+++ core/src/main/java/io/jstach/rainbowgum/LogOutput.java
@@ -324,7 +324,7 @@ public interface LogOutput extends LogLifecycle, Flushable, LogComponent {
 
 	/**
 	 * Attempts to reopen the output if supported and <strong>SHOULD only be called by the
-	 * appender!</strong> This call is mainly used for external log rotation systems such
+	 * appender</strong>! This call is mainly used for external log rotation systems such
 	 * as logrotate or an aggregator agent.
 	 * @return status and the default implementation will return
 	 * {@link LogResponse.Status.StandardStatus#IGNORED}.
