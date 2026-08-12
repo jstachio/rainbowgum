c570bf3eed54773dbeac0d320826b48b98671c01
Adam Gent <adam.gent@snaphop.com>
2026-08-11 20:19:36 -0400

    Remove unused level mapping code

--- rainbowgum-slf4j/src/main/java/io/jstach/rainbowgum/slf4j/Levels.java
+++ rainbowgum-slf4j/src/main/java/io/jstach/rainbowgum/slf4j/Levels.java
@@ -1,28 +1,10 @@
 package io.jstach.rainbowgum.slf4j;
 
-import static org.slf4j.event.EventConstants.DEBUG_INT;
-import static org.slf4j.event.EventConstants.ERROR_INT;
-import static org.slf4j.event.EventConstants.INFO_INT;
-import static org.slf4j.event.EventConstants.TRACE_INT;
-import static org.slf4j.event.EventConstants.WARN_INT;
-
 import org.slf4j.event.Level;
 
 class Levels {
 
 	static final int OFF_INT = Integer.MIN_VALUE;
-
-	static int toSlf4jInt(System.Logger.Level level) {
-		return switch (level) {
-			case DEBUG -> DEBUG_INT;
-			case ALL -> TRACE_INT;
-			case ERROR -> ERROR_INT;
-			case INFO -> INFO_INT;
-			case OFF -> OFF_INT;
-			case TRACE -> TRACE_INT;
-			case WARNING -> WARN_INT;
-		};
-	}
 
 	static Level toSlf4jLevel(System.Logger.Level level) {
 		return switch (level) {
