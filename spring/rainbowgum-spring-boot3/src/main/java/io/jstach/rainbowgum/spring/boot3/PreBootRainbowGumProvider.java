033fba8097bdc1417ad7393d1299c41de9b64725
Adam Gent <adam.gent@snaphop.com>
2026-08-24 14:49:50 -0400

    Force Spring Boot not to use Rainbow Gums JUL

--- spring/rainbowgum-spring-boot3/src/main/java/io/jstach/rainbowgum/spring/boot3/PreBootRainbowGumProvider.java
+++ spring/rainbowgum-spring-boot3/src/main/java/io/jstach/rainbowgum/spring/boot3/PreBootRainbowGumProvider.java
@@ -1,11 +1,14 @@
 package io.jstach.rainbowgum.spring.boot3;
 
+import java.util.List;
 import java.util.Optional;
 
 import io.jstach.rainbowgum.LogConfig;
 import io.jstach.rainbowgum.LogProperties;
 import io.jstach.rainbowgum.RainbowGum;
+import io.jstach.rainbowgum.ServiceRegistry;
 import io.jstach.rainbowgum.spi.RainbowGumServiceProvider;
+import io.jstach.rainbowgum.spi.RainbowGumServiceProvider.PropertiesProvider;
 import io.jstach.rainbowgum.spi.RainbowGumServiceProvider.RainbowGumProvider;
 import io.jstach.svc.ServiceProvider;
 
@@ -13,7 +16,7 @@ import io.jstach.svc.ServiceProvider;
  * Provides a Rainbow Gum that is used during the pre boot process of Spring.
  */
 @ServiceProvider(RainbowGumServiceProvider.class)
-public class PreBootRainbowGumProvider implements RainbowGumProvider {
+public class PreBootRainbowGumProvider implements RainbowGumProvider, PropertiesProvider {
 
 	/**
 	 * For service loader.
@@ -23,29 +26,39 @@ public class PreBootRainbowGumProvider implements RainbowGumProvider {
 
 	@Override
 	public Optional<RainbowGum> provide(LogConfig config) {
-		/*
-		 * TODO we really need to make this programmatic
-		 */
-		String changeProperties = """
-				logging.global.change=true
-				logging.change=level
-				""";
-
-		var properties = LogProperties.builder()
-			// .fromFunction(System::getProperty)
-			// .removeKeyPrefix("boot.logging.")
-			.fromProperties(changeProperties)
-			.description("PreBoot properties")
-			.build();
-		var newConfig = LogConfig.builder().properties(properties).build();
-		newConfig.serviceRegistry().put(BootFlag.class, BootFlag.INSTANCE);
-		return Optional.of(RainbowGum.queued(newConfig));
+		config.serviceRegistry().put(BootFlag.class, BootFlag.INSTANCE);
+		return Optional.of(RainbowGum.queued(config));
 	}
 
 	enum BootFlag {
 
 		INSTANCE;
 
+	}
+
+	@Override
+	public List<LogProperties> provideProperties(ServiceRegistry registry) {
+		/*
+		 * We need to disable JUL because for whatever reasons rainbowgums version is slow
+		 * on Spring Boot.
+		 *
+		 * We also need to have changing level loggers so we do not miss anything.
+		 *
+		 * However any loggers used after Spring Boots initialization should be level
+		 * loggers assuming configuration is not different.
+		 */
+		String changeProperties = """
+				logging.jul.disable=true
+				logging.global.change=true
+				logging.change=level
+				""";
+
+		var properties = LogProperties.builder()
+			.fromProperties(changeProperties)
+			.description("PreBoot properties")
+			.build();
+
+		return List.of(LogProperties.StandardProperties.SYSTEM_PROPERTIES, properties);
 	}
 
 }
