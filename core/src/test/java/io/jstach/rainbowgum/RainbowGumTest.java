2f97062873c4edd4fea926be2233feb40f00649b
Adam Gent <adam.gent@snaphop.com>
2026-09-17 21:15:54 -0400

    Remove commented-out code in RainbowGumTest.java
    Two entire dead test methods (testBuilder, testLevelConfig) superseded
    by testAsyncPublisher, plus two dead leftover lines inside it (an unused
    LogConfig.of() var and a one-liner replaced by the manual isEnabled()
    check right below it).

--- core/src/test/java/io/jstach/rainbowgum/RainbowGumTest.java
+++ core/src/test/java/io/jstach/rainbowgum/RainbowGumTest.java
@@ -22,40 +22,8 @@ import io.jstach.rainbowgum.LogPublisher.PublisherFactory;
 @Execution(ExecutionMode.SAME_THREAD)
 class RainbowGumTest {
 
-	// @Test
-	// public void testBuilder() throws Exception {
-	// var gum = RainbowGum.builder().build();
-	// gum.start();
-	// var logger = gum.router().getLogger("stuff");
-	// logger.log(Level.INFO, "Stuff");
-	// var router = gum.router();
-	// var level = router.levelResolver().resolveLevel("stuff");
-	// var out = Objects.requireNonNull(System.out);
-	// out.println(level);
-	// boolean actual = router.route("stuff", Level.WARNING).isEnabled();
-	// assertTrue(actual);
-	// assertFalse(gum.router().route("stuff", Level.DEBUG).isEnabled());
-	//
-	// }
-	//
-	// @Test
-	// public void testLevelConfig() throws Exception {
-	// Map<String, String> config = Map.of("logging.level.stuff", "" +
-	// Level.DEBUG.name());
-	// var gum = RainbowGum.builder(b -> b.properties(config::get)).build();
-	//
-	// var logger = gum.router().getLogger("stuff");
-	//
-	// logger.log(Level.DEBUG, "Stuff");
-	// assertFalse(logger.isLoggable(Level.TRACE));
-	//
-	// assertTrue(logger.isLoggable(Level.DEBUG));
-	//
-	// }
-
 	@Test
 	void testAsyncPublisher() throws Exception {
-		// var config = LogConfig.of();
 		var formatter = LogFormatter.builder() //
 			.timeStamp() //
 			.space() //
@@ -75,7 +43,6 @@ class RainbowGumTest {
 			.formatter(formatter)
 			.build();
 
-		// GlobalLogRouter.INSTANCE.log("stuff", Level.WARNING, "first");
 		{
 			var route = GlobalLogRouter.INSTANCE.route("stuff", Level.WARNING);
 			if (route.isEnabled()) {
